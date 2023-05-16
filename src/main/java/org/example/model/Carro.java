c6117e305d467a4a184feb3314793ab7995bcdc1
fabith95 <fabith95@gmail.com>
2023-05-15 23:58:59 -0500

    Agregar metodo frenar

--- src/main/java/org/example/model/Carro.java
+++ src/main/java/org/example/model/Carro.java
@@ -6,7 +6,8 @@ public class Carro {
     private int cambioVelocidad;
     public static int MAX_VELOCIDAD = 100;
 
-    public int getVelocidad(){
+
+    public int getVelocidad() {
 
         return velocidad;
     }
@@ -15,18 +16,31 @@ public class Carro {
         Carro carro = new Carro();
 
         if (carro.velocidad == 0) {
-            cambioVelocidad = 10;
+            this.cambioVelocidad = 10;
         }
-        return cambioVelocidad;
+        return this.cambioVelocidad;
     }
 
     public void acelerar() {
 
-        if (velocidad < MAX_VELOCIDAD){
-        this.velocidad += 10;
-        }else {
-            throw new RuntimeException("No debes superar la velocidad " + MAX_VELOCIDAD);
+        if (velocidad < MAX_VELOCIDAD) {
+            this.velocidad += 10;
+        } else {
+            throw new RuntimeException("No debes superar la velocidad: " + MAX_VELOCIDAD);
+        }
+    }
+
+    public void decelerar() {
+
+        if (velocidad < MAX_VELOCIDAD) {
+            this.velocidad += 10;
+        }
+        if (velocidad < MAX_VELOCIDAD) {
+            this.velocidad -= 10;
         }
 
+
     }
+
+
 }
