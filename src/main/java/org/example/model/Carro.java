eaab0b1421ce28e53d4d8f2291ecfc383ba2e968
fabith95 <fabith95@gmail.com>
2023-05-16 17:39:52 -0500

    Modificar y se agrega metodo disminuir velocidad

--- src/main/java/org/example/model/Carro.java
+++ src/main/java/org/example/model/Carro.java
@@ -8,10 +8,6 @@ public class Carro {
     private int cambioVelocidad;
     public static int MAX_VELOCIDAD = 100;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> c6117e305d467a4a184feb3314793ab7995bcdc1
     public int getVelocidad() {
 
         return velocidad;
@@ -32,31 +28,12 @@ public class Carro {
             this.velocidad += 10;
         } else {
             throw new RuntimeException("No debes superar la velocidad: " + MAX_VELOCIDAD);
-<<<<<<< HEAD
         }
     }
 
     public void decelerar(int deceleracion) {
-
         velocidad -= deceleracion;
     }
 
-=======
-        }
-    }
-
-    public void decelerar() {
-
-        if (velocidad < MAX_VELOCIDAD) {
-            this.velocidad += 10;
-        }
-        if (velocidad < MAX_VELOCIDAD) {
-            this.velocidad -= 10;
-        }
-
-
-    }
-
->>>>>>> c6117e305d467a4a184feb3314793ab7995bcdc1
 
 }
